2c925c07b1c53737aae8ab836ba606e570cc99f7
Dirk Fröhling <dirk.froehling@w-hs.de>
2022-06-29 23:36:12 +0200

    Work in progress

--- src/NanoBLEFlashPrefs.cpp
+++ src/NanoBLEFlashPrefs.cpp
@@ -3,98 +3,107 @@
 
 // See https://infocenter.nordicsemi.com/index.jsp?topic=%2Fcom.nordic.infocenter.sdk5.v15.0.0%2Flib_fstorage.html
 
-#define FILE_ID		0x0001  /* The ID of the file to write the records into. */
-#define RECORD_KEY	0x1111  /* A key for the preferences record. */
-
+#define FILE_ID 0x0001    /* The ID of the file to write the records into. */
+#define RECORD_KEY 0x1111 /* A key for the preferences record. */
 
 /* Array to map FDS return values to strings. */
-char const * fds_err_str[] =
-{
-    "FDS_SUCCESS",
-    "FDS_ERR_OPERATION_TIMEOUT",
-    "FDS_ERR_NOT_INITIALIZED",
-    "FDS_ERR_UNALIGNED_ADDR",
-    "FDS_ERR_INVALID_ARG",
-    "FDS_ERR_NULL_ARG",
-    "FDS_ERR_NO_OPEN_RECORDS",
-    "FDS_ERR_NO_SPACE_IN_FLASH",
-    "FDS_ERR_NO_SPACE_IN_QUEUES",
-    "FDS_ERR_RECORD_TOO_LARGE",
-    "FDS_ERR_NOT_FOUND",
-    "FDS_ERR_NO_PAGES",
-    "FDS_ERR_USER_LIMIT_REACHED",
-    "FDS_ERR_CRC_CHECK_FAILED",
-    "FDS_ERR_BUSY",
-    "FDS_ERR_INTERNAL",
+char const *fds_err_str[] =
+    {
+        "FDS_SUCCESS",
+        "FDS_ERR_OPERATION_TIMEOUT",
+        "FDS_ERR_NOT_INITIALIZED",
+        "FDS_ERR_UNALIGNED_ADDR",
+        "FDS_ERR_INVALID_ARG",
+        "FDS_ERR_NULL_ARG",
+        "FDS_ERR_NO_OPEN_RECORDS",
+        "FDS_ERR_NO_SPACE_IN_FLASH",
+        "FDS_ERR_NO_SPACE_IN_QUEUES",
+        "FDS_ERR_RECORD_TOO_LARGE",
+        "FDS_ERR_NOT_FOUND",
+        "FDS_ERR_NO_PAGES",
+        "FDS_ERR_USER_LIMIT_REACHED",
+        "FDS_ERR_CRC_CHECK_FAILED",
+        "FDS_ERR_BUSY",
+        "FDS_ERR_INTERNAL",
 };
-
 
 /* Array to map FDS events to strings. */
-static char const * fds_evt_str[] =
-{
-    "FDS_EVT_INIT",
-    "FDS_EVT_WRITE",
-    "FDS_EVT_UPDATE",
-    "FDS_EVT_DEL_RECORD",
-    "FDS_EVT_DEL_FILE",
-    "FDS_EVT_GC",
+static char const *fds_evt_str[] =
+    {
+        "FDS_EVT_INIT",
+        "FDS_EVT_WRITE",
+        "FDS_EVT_UPDATE",
+        "FDS_EVT_DEL_RECORD",
+        "FDS_EVT_DEL_FILE",
+        "FDS_EVT_GC",
 };
-
 
 // Definition of static class members
 
 bool NanoBLEFlashPrefs::opCompleted;
 
+ret_code_t NanoBLEFlashPrefs::lastResult = NRF_SUCCESS;
 
-// Simple event handler to handle errors.
-void NanoBLEFlashPrefs::fdsEventHandler(fds_evt_t const * fdsEvent)
+// Simple event handler
+void NanoBLEFlashPrefs::fdsEventHandler(fds_evt_t const *fdsEvent)
 {
+  opCompleted = true;
+  lastResult = fdsEvent->result;
 
-  if (fdsEvent->result == NRF_SUCCESS) {
-		opCompleted = true;
-    } else {
-		opCompleted = false;
-	}
-
-  switch (fdsEvent->id) {
-      case FDS_EVT_INIT:
-          break;
-      case FDS_EVT_WRITE:
-          break;
-      case FDS_EVT_UPDATE:
-          break;
-      case FDS_EVT_DEL_RECORD:
-          break;
-      default:
-          break;
+  switch (fdsEvent->id)
+  {
+  case FDS_EVT_INIT:
+    break;
+  case FDS_EVT_WRITE:
+    break;
+  case FDS_EVT_UPDATE:
+    break;
+  case FDS_EVT_DEL_RECORD:
+    break;
+  case FDS_EVT_DEL_FILE:
+    break;
+  case FDS_EVT_GC:
+    break;
+  default:
+    break;
   }
 }
-
 
 // Constructor
 NanoBLEFlashPrefs::NanoBLEFlashPrefs()
 {
-	opCompleted = false;
-	
-	ret_code_t ret = fds_register(fdsEventHandler);
-	if (ret != NRF_SUCCESS) {
-	    // Registering of the FDS event handler has failed.
-	}
-	
-	ret = fds_init();
-	if (ret != NRF_SUCCESS) {
-    	// Handle error.
-	}
-	
-}
+  opCompleted = false;
 
+  ret_code_t ret = fds_register(fdsEventHandler);
+  if (ret != NRF_SUCCESS)
+  {
+    // Registering of the FDS event handler has failed.
+  }
+
+  ret = fds_init();
+  if (ret != NRF_SUCCESS)
+  {
+    // Handle error.
+  }
+
+  // Wait for initialization
+  while (!opCompleted)
+  {
+    /* wait */
+  }
+}
 
 int8_t NanoBLEFlashPrefs::writePrefs(void *value, int lengthInByte)
 {
-	fds_record_t        record;
-	fds_record_desc_t   record_desc;
+  fds_record_t record;
+  fds_record_desc_t record_desc;
+  fds_find_token_t ftok;
 
-  // Must be statically allocated, because it is going to be written in flash.
+  // It is required to zero the token before first use.
+  memset(&ftok, 0x00, sizeof(fds_find_token_t));
+
+  // Must be statically allocated, because it is going to be written to flash memory.
+  // (We wait for the operation to complete, so probably not neccesary)
   static uint8_t m_data[4076];
 
   opCompleted = false; // Will be set to true in event handler in case of NRF_SUCCESS
@@ -105,85 +114,132 @@ int8_t NanoBLEFlashPrefs::writePrefs(void *value, int lengthInByte)
   memcpy(m_data, value, len);
 
   // Set up record.
-	record.file_id           = FILE_ID;
-	record.key               = RECORD_KEY;
-	record.data.p_data       = value;
-	// Calculate length in words, take into account any eventual remainder of the division.
-	record.data.length_words = (len + 3) / 4;
+  record.file_id = FILE_ID;
+  record.key = RECORD_KEY;
+  record.data.p_data = m_data;
+  // Calculate length in words, take into account any eventual remainder of the division.
+  record.data.length_words = (len + 3) / 4;
+  Serial.println(record.data.length_words);
 
-	return fds_record_write(&record_desc, &record);
+  if (fds_record_find(FILE_ID, RECORD_KEY, &record_desc, &ftok) == NRF_SUCCESS)
+  {
+    Serial.println("UPDATE!");
+    ret_code_t ret = fds_record_update(&record_desc, &record);
+    Serial.println(errorString(ret));
+  }
+  else
+  {
+    Serial.println("WRITE!");
+    ret_code_t ret = fds_record_write(&record_desc, &record);
+    Serial.println(errorString(ret));
+  }
+
+  while (!opCompleted)
+  {
+    // Serial.println("fgh");
+    /* wait */
+  }
+
+  return lastResult;
 }
 
-
-int8_t NanoBLEFlashPrefs::readPrefs(void* value, int lengthInByte)
+int8_t NanoBLEFlashPrefs::readPrefs(void *value, int lengthInByte)
 {
-	fds_flash_record_t  flash_record;
-	fds_record_desc_t   record_desc;
-	fds_find_token_t    ftok;
-	
-	// It is required to zero the token before first use.
-	memset(&ftok, 0x00, sizeof(fds_find_token_t));
-	
-	ret_code_t rc = FDS_ERR_NOT_FOUND;
-	// Loop until all records with the given key and file ID have been found.
-	while (fds_record_find(FILE_ID, RECORD_KEY, &record_desc, &ftok) == NRF_SUCCESS) {
-		rc = fds_record_open(&record_desc, &flash_record);
-		if (rc != NRF_SUCCESS) { // leave while loop in case of problems
+  fds_flash_record_t flash_record;
+  fds_record_desc_t record_desc;
+  fds_find_token_t ftok;
+
+  // It is required to zero the token before first use.
+  memset(&ftok, 0x00, sizeof(fds_find_token_t));
+
+  ret_code_t rc = FDS_ERR_NOT_FOUND;
+  // Loop until all records with the given key and file ID have been found.
+  while (fds_record_find(FILE_ID, RECORD_KEY, &record_desc, &ftok) == NRF_SUCCESS)
+  {
+    rc = fds_record_open(&record_desc, &flash_record);
+    if (rc != NRF_SUCCESS)
+    { // leave while loop in case of problems
       return rc;
     }
 
     // Access the record through the flash_record structure.
-		// uint16_t length = flash_record.p_header.length_words;
-		// lengthInByte = length * 4;
-		memcpy(value, flash_record.p_data, lengthInByte);
-		
-		// Close the record when done.
-		rc = fds_record_close(&record_desc);
-	}
-	
-	return rc;
-}
+    // uint16_t length = flash_record.p_header.length_words;
+    // lengthInByte = length * 4;
+    memcpy(value, flash_record.p_data, lengthInByte);
 
-
-int8_t NanoBLEFlashPrefs::deletePrefs()
-{
-	fds_record_desc_t   record_desc;
-	fds_find_token_t    ftok;
-
-  opCompleted = false; // Will be set to true in event handler in case of NRF_SUCCESS
-
-  // It is required to zero the token before first use.
-	memset(&ftok, 0x00, sizeof(fds_find_token_t));
-
-  ret_code_t rc;
-
-  // Loop until all records with the given key and file ID have been found.
-  while (fds_record_find(FILE_ID, RECORD_KEY, &record_desc, &ftok) == NRF_SUCCESS) {
-    rc = fds_record_delete(&record_desc);
-		if (rc != FDS_SUCCESS) {  // leave while loop in case of problems
-      return rc;
-    }
+    // Close the record when done.
+    rc = fds_record_close(&record_desc);
   }
 
   return rc;
 }
 
+int8_t NanoBLEFlashPrefs::deletePrefs()
+{
+  fds_record_desc_t record_desc;
+  fds_find_token_t ftok;
+
+  // It is required to zero the token before first use.
+  memset(&ftok, 0x00, sizeof(fds_find_token_t));
+
+  ret_code_t rc;
+
+  // Loop until all records with the given key and file ID have been found.
+  while (fds_record_find(FILE_ID, RECORD_KEY, &record_desc, &ftok) == NRF_SUCCESS)
+  {
+    opCompleted = false; // Will be set to true in event handler
+    rc = fds_record_delete(&record_desc);
+    if (rc != FDS_SUCCESS)
+    { // leave while loop in case of problems
+      break;
+    }
+    while (!opCompleted)
+    {
+      /* wait */
+    }
+  }
+
+  return lastResult;
+}
 
 int8_t NanoBLEFlashPrefs::garbageCollection()
 {
-	return fds_gc();
+  opCompleted = false; // Will be set to true in event handler
+  fds_gc();
+  while (!opCompleted)
+  {
+    /* wait */
+  }
+
+  return lastResult;
 }
 
+int8_t NanoBLEFlashPrefs::getStatistics()
+{
+  fds_stat_t stats;
+  fds_stat(&stats);
+
+  Serial.print("pages_available ");
+  Serial.println(stats.pages_available);
+  Serial.print("open_records ");
+  Serial.println(stats.open_records);
+  Serial.print("valid_records ");
+  Serial.println(stats.valid_records);
+  Serial.print("dirty_records ");
+  Serial.println(stats.dirty_records);
+  Serial.print("corruption ");
+  Serial.println(stats.corruption);
+}
 
 bool NanoBLEFlashPrefs::operationCompleted()
 {
-	return opCompleted;
+  return opCompleted;
 }
-
 
 const char *NanoBLEFlashPrefs::errorString(int8_t returnCode)
 {
-  if (returnCode >= 0 && returnCode < 16) {
+  if (returnCode >= 0 && returnCode < 16)
+  {
     return fds_err_str[returnCode];
   }
 
--- src/NanoBLEFlashPrefs.h
+++ src/NanoBLEFlashPrefs.h
@@ -5,63 +5,65 @@
 
 class NanoBLEFlashPrefs
 {
-  	public:
-      /**
-       * Constructor. Initializes the Flash Data Storage (FDS) module.
-       */
-      NanoBLEFlashPrefs();
+public:
+  /**
+   * Constructor. Initializes the Flash Data Storage (FDS) module.
+   */
+  NanoBLEFlashPrefs();
 
-      /**
-       * Write any preference variable or struct to flash file system.
-       *
-       * @param value Any kind of variable, preferably a struct
-       * @param lengthInByte Length of value in byte
-       * @return Return code of Flash Data Storage module.
-       */
-      int8_t writePrefs(void *value, int lengthInByte);
+  /**
+   * Write any preference variable or struct to flash file system.
+   *
+   * @param value Any kind of variable, preferably a struct
+   * @param lengthInByte Length of value in byte
+   * @return Return code of Flash Data Storage module.
+   */
+  int8_t writePrefs(void *value, int lengthInByte);
 
-      /**
-       * Read content of preference record into value.
-       *
-       * @param value Any kind of variable, preferably a struct
-       * @param lengthInByte Length of value in byte
-       * @return Return code of Flash Data Storage module.
-       */
-      int8_t readPrefs(void *value, int lengthInByte);
+  /**
+   * Read content of preference record into value.
+   *
+   * @param value Any kind of variable, preferably a struct
+   * @param lengthInByte Length of value in byte
+   * @return Return code of Flash Data Storage module.
+   */
+  int8_t readPrefs(void *value, int lengthInByte);
 
-      /**
-       * Deletes (actually invalidates) preference record.
-       *
-       * @return Return code of Flash Data Storage module.
-       */
-      int8_t deletePrefs();
+  /**
+   * Deletes (actually invalidates) preference record.
+   *
+   * @return Return code of Flash Data Storage module.
+   */
+  int8_t deletePrefs();
 
-      /**
-       * Clears file system from all invalid preference records.
-       *
-       * @return Return code of Flash Data Storage module.
-       */
-      int8_t garbageCollection();
+  /**
+   * Clears file system from all invalid preference records.
+   *
+   * @return Return code of Flash Data Storage module.
+   */
+  int8_t garbageCollection();
 
-      /**
-       * Test if writePrefs or deletePrefs is done. (Initialization, 
-       * writing or erasing flash memory are asynchronous operations)
-       *
-       * @return TRUE if successfully completed, FALSE if not.
-       */
-      bool operationCompleted();
+  int8_t getStatistics();
 
-      /**
-       * Returns a meaningful error string when given a valid return code.
-       *
-       * @return Error string from Flash Data Storage module.
-       */
-      const char *errorString(int8_t rc);
+  /**
+   * Test if writePrefs or deletePrefs is done. (Initialization,
+   * writing or erasing flash memory are asynchronous operations)
+   *
+   * @return TRUE if successfully completed, FALSE if not.
+   */
+  bool operationCompleted();
 
-    private:
-  		static bool opCompleted;
-		  static void fdsEventHandler(fds_evt_t const * fdsEvent);
-  		
+  /**
+   * Returns a meaningful error string when given a valid return code.
+   *
+   * @return Error string from Flash Data Storage module.
+   */
+  const char *errorString(int8_t rc);
+
+private:
+  static bool opCompleted;
+  static ret_code_t lastResult;
+  static void fdsEventHandler(fds_evt_t const *fdsEvent);
 };
 
 #endif
